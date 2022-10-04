dd76c98d176f1c1b8f13f608607b4e0b9adaaa86
ahmed gamel <97199823+Ahmedigamel7@users.noreply.github.com>
2022-10-05 01:39:32 +0200

    Time: 7 ms (13.68%), Space: 5.9 MB (46.92%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -4,14 +4,14 @@ public:
     
 // Recursive Version 
       
-     /*   if (n == 0) return 1;
+        if (n == 0) return 1;
 
        int absN = abs(n);
 
 double answer = (absN % 2 == 0) ? myPow(x * x, absN / 2) : myPow(x * x, (absN - 1) / 2) * x ;
 
 	return (n > 0) ? answer : 1 / answer;
-        */
+        
         
 // Iterative Version        
 
