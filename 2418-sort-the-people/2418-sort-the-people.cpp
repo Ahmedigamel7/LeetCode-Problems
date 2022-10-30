cd02c3320b5982287242d1bde2cbd68a0bc163ad
ahmed gamel <97199823+Ahmedigamel7@users.noreply.github.com>
2022-10-30 17:52:31 +0200

    Update 2418-sort-the-people.cpp

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
   vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+   // better Time complexity
     vector<pair<int,string>>hash;
         vector<string>res;
         for(int i=0; i<names.size(); i++) {
@@ -13,3 +14,17 @@ public:
         return res;
    }
 };
+/*
+    // better Memory & Terrible O(N^2) Runtime 
+              for(int i=1;i<heights.size();i++){
+                for(int j=0;j<heights.size()-i;j++){
+                  if(heights[j] < heights[j+1]){
+                    swap(heights[j],heights[j+1]);
+                    swap(names[j],names[j+1]);
+                }
+            }
+        }
+        return names; 
+  }
+   */
+ 
